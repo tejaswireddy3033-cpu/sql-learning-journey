b6d34765b818c805885eb1e06a2e3bab3eb83b0d
tejaswireddy3033-cpu <tejaswireddy3033@gmail.com>
2026-08-31 20:33:10 +0530

    Add basic SQL queries

---
+++ basic_queries.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE college;
+USE college;
+CREATE TABLE students(
+  id INT,
+  name VARCHAR(50),
+  age INT,
+  department VARCHAR(50)
+  );
+INSERT INTO students VALUES
+(1, 'Anu', 20, 'CSE')
+(2, 'Rahul', 21, 'ECE')
+(3, 'Priya', 20, 'RAI')
+SELECT FROM students;
